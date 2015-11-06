72044d90a3598947149cdba8e8a7cc163cadcd98
kevin <kevin_22894@hotmail.com>
2015-11-06 02:33:37 -0500

    Fix. Modelos

--- apps/atenciones/admin.py
+++ apps/atenciones/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
+
+class AtencionClinica_DetalleProductosAdmin(admin.TabularInline):
+    model = AtencionClinica_DetalleProductos
+    list_display = ('atencion_clinica','codigo_producto','cantidad',)
+    extra = 1
+
+class AtencionClinica_DetalleMedicinaAdmin(admin.TabularInline):
+    model = AtencionClinica_DetalleMedicina
+    list_display = ('atencion_clinica','medicina','dosis',)
+    extra = 1
+
 class AtencionClinicaAdmin(admin.ModelAdmin):
     model = AtencionClinica
     list_display = ('fecha','mascota','veterinario','diagnostico','proxima_cita','observaciones')
+    inlines =[AtencionClinica_DetalleProductosAdmin,AtencionClinica_DetalleMedicinaAdmin,]
 
 class CitaAdmin(admin.ModelAdmin):
     model = Cita
@@ -11,3 +23,5 @@ class CitaAdmin(admin.ModelAdmin):
 
 admin.site.register(AtencionClinica,AtencionClinicaAdmin)
 admin.site.register(Cita,CitaAdmin)
+#admin.site.register(AtencionClinica_DetalleMedicina)
+#admin.site.register(AtencionClinica_DetalleProductos)
--- apps/atenciones/models.py
+++ apps/atenciones/models.py
@@ -3,11 +3,14 @@ from django.db import models
 from apps.trabajador.models import Veterinario
 from apps.mascota.models import Mascota
 from django.utils import timezone
+from apps.vacuna.models import Medicina
+from apps.servicios.models import Producto
 # Create your models here.
 class Cita(models.Model):
     veterinario = models.ForeignKey(Veterinario)
     mascota = models.ForeignKey(Mascota)
     fecha = models.DateField()
+    atendido = models.BooleanField(default=False)
     def __unicode__(self):
         return "%s - %s" %(self.fecha,self.mascota.nombre)
 
@@ -17,9 +20,27 @@ class AtencionClinica(models.Model):
     fecha = models.DateField(default=timezone.now())
     diagnostico = models.TextField()
     proxima_cita = models.ForeignKey(Cita,blank=True, null=True)
+    medicinas = models.ManyToManyField(Medicina,blank=True,null=True)
+    productos = models.ManyToManyField(Producto,blank=True,null=True)
     observaciones = models.TextField(blank=True, null=True)
     def __unicode__(self):
         return "%s - %s" %(self.fecha,self.mascota.nombre)
     class Meta:
         verbose_name = 'Atención Clínica'
         verbose_name_plural = 'Atenciones Clinicas'
+
+class AtencionClinica_DetalleProductos(models.Model):
+    atencion_clinica = models.ForeignKey(AtencionClinica)
+    codigo_producto = models.ForeignKey(Producto)
+    cantidad = models.SmallIntegerField()
+    class Meta:
+        verbose_name = 'Detalle - Producto'
+        verbose_name_plural = 'Detalle - Productos'
+
+class AtencionClinica_DetalleMedicina(models.Model):
+    atencion_clinica = models.ForeignKey(AtencionClinica,on_delete=models.CASCADE)
+    medicina = models.ForeignKey(Medicina,on_delete=models.CASCADE)
+    dosis = models.CharField(max_length=15)
+    class Meta:
+        verbose_name = 'Composición'
+        verbose_name_plural = 'Medicamentos'
